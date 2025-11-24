1a4096a14dc172d12e4ba4dab3f33ac759b9fd6e
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-24 17:53:38 -0300

    Clase 22

--- clase-21/src/App.jsx
+++ clase-21/src/App.jsx
@@ -10,20 +10,49 @@ EJ: <CustomComponent/>
 function App() {
 
   let titulo = <h1>Hola mundo</h1>
+
+  const products = [
+    { title: "Tv LG 22\"", price: 4000, id: 1},
+    { title: "Tv Noblex 34\"", price: 3000, id: 2 },
+    { title: "Tv samsung 42\"", price: 5000, id: 3 },
+    { title: "Tv samsung 62\"", price: 6000, id: 4 }
+  ]
+  /* 
+  Opcion 1 (poco usado) FOR OF
+  const products_jsx = []
+
+  for (const product of products) {
+    products_jsx.push(
+      <ProductCard title={product.title} price={product.price} />
+    )
+  } 
+  */
+
+  //Opcion 2 (RECOMENDADA) usar map
+  /* 
+  map es un metodo de arrays (osea solo se puede aplicar sobre un array)
+  map sirve para crear un nuevo array a partir del original
+  map internamente tiene un for of y por cada elemento del array ejecutara la callback
+  Lo que retorne la callback de map sera el nuevo valor del elemento
+  MAP ES UN METODO NO MUTABLE, NO MODIFICA EL ARRAY ORIGINAL
+  */
+  const products_list_JSX = products.map(
+    function (product){
+      return <ProductCard title={product.title} price={product.price} key={product.id}/>
+    }
+  )
+
+  console.log({products_list_JSX})
+
+
+
   return (
     <div>
       {titulo}
       <button>Hola</button>
-      <ProductCard 
-        title={'Tv LG 22\"'} 
-        price={4000} 
-        has_discount 
-      />
-      <ProductCard title={"Tv Noblex 34\""} price={3000} />
-      <ProductCard title={"Tv samsung 42\""} price={5000}/>
-      <ProductCard title={"Tv samsung 62\""} price={6000}/>
-      <MessagesContainer/>
-    </div>  
+      {products_list_JSX}
+      {/* <MessagesContainer/> */}
+    </div>
   )
 }
 
@@ -32,7 +61,7 @@ function App() {
 
 /* CustomComponent */
 
-function CustomComponent (){
+function CustomComponent() {
   return (
     <h1>Soy un componente</h1>
   )
